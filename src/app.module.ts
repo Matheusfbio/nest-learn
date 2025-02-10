1588ce19b56aa8d986b6409a3465731557a674fb
Matheus Fábio <matheusfabiors@gmail.com>
2025-02-10 00:25:40 -0300

    feat: building the user api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserService } from './user/user.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { DatabaseModule } from './database/database.module';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
-  imports: [AuthModule, UserModule],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService],
+  imports: [AuthModule, UserModule, DatabaseModule],
+  controllers: [UserController],
+  providers: [UserService, PrismaService],
 })
 export class AppModule {}
